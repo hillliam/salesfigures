05d668884f723e7e5f7ea077848a24b8178fee5c
hillliam <liamchat@gmail.com>
2018-03-21 16:24:29 +0000

    move product code into product file

--- figures/src/Products.hs
+++ figures/src/Products.hs
@@ -14,5 +14,27 @@
 
 module Products where
 
+import JSONParser_solution
 
+-- id key
+-- name string
+-- description string
+-- variants type of tshirt
+-- list of prices
+-- list of options
+-- sku product code
 
+readproducts :: IO ()
+readproducts = do
+      input <- readFile "products.json"
+      let output = read input
+      putStrLn (show (output:: JValue))
+      putStrLn  (unwords (loadproducts (output:: JValue)))
+
+loadproducts :: JValue -> [String]
+loadproducts (JArray item) = map loadproduct item
+
+loadproduct :: JValue -> String
+loadproduct (JObject item) = do
+  let value = [ if k == "name" then v else "" | (k, JString v) <- item]
+  unlines value
