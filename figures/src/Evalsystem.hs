4f1daf5705c9283b480cf07c115c57a947a78385
hillliam <liamchat@gmail.com>
2018-03-30 21:35:41 +0100

    file to handle runtime evaluation of Haskell

---
+++ figures/src/Evalsystem.hs
@@ -0,0 +1,23 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  Evalsystem
+-- Copyright   :  2018 liam hill
+-- License     :  BSD3
+--
+-- Maintainer  :  example@example.com
+-- Stability   :
+-- Portability :
+--
+-- |removed because it makes testing harder
+--
+-----------------------------------------------------------------------------
+
+module Evalsystem where
+
+--import System.Eval.Haskell
+
+--evaltest :: String -> String
+--evaltest a = do
+--   i <- eval a [] :: IO (Maybe Int)
+--   when (isJust i) $ putStrLn (show (fromJust i))
+
