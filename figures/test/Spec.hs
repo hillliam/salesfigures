ea7958a35c0761b6cc395021b0619378026a06d2
hillliam <liamchat@gmail.com>
2018-04-05 14:16:06 +0100

    add tests

--- figures/test/Spec.hs
+++ figures/test/Spec.hs
@@ -1,2 +1,21 @@
+import Test.HUnit
+import Quckcheck
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    testproduct
+    testorders
+
+testproduct :: IO()
+testproduct = do
+    input <- readFile "products.json"
+    let output = read input
+    testpcount
+
+testorders :: IO()
+testorders = do
+    input <- readFile "orders.json"
+    let output = read input
+
+testpcount :: Jvalue -> Bool
+testpcount =
