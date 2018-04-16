98bd3aaecf28a749f78c92f90c1465d9fe39f766
hillliam <liamchat@gmail.com>
2018-04-17 00:03:38 +0100

    started to make database version

---
+++ figures/src/Databaseversion.hs
@@ -0,0 +1,25 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  Databaseversion
+-- Copyright   :  2018 liam hill
+-- License     :  BSD3
+--
+-- Maintainer  :  example@example.com
+-- Stability   :
+-- Portability :
+--
+-- handles the database version of the JSON files
+--
+-----------------------------------------------------------------------------
+
+module Databaseversion where
+
+import Database.HDBC
+--import Database.HDBC.Sqlite3
+
+populatedatabase :: IO ()
+populatedatabase = undefined
+
+performquerey :: String -> IO ()
+performquerey command = undefined
+
